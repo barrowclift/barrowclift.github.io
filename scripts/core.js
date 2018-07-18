86271b3144504f59082191ce328e8af6df8585c6
Marc Barrowclift <Marc_Barrowclift@comcast.com>
2018-07-18 08:39:58 -0400

    Fixing Firefox bug with dismissing the Settings popup

--- scripts/core.js
+++ scripts/core.js
@@ -217,7 +217,7 @@ $(document).ready(function() {
 	// https://stackoverflow.com/questions/11406285/determine-and-bind-click-or-touch-event
 	var clickEventName = (('ontouchstart' in window) || (window.DocumentTouch && document instanceof DocumentTouch)) ? 'touchstart' : 'click';
 	// https://stackoverflow.com/a/3028037
-	$(document).on(clickEventName, function() {
+	$(document).on(clickEventName, function(event) {
 		if ($("#settings-popup").hasClass("show")) {
 			if (!$(event.target).closest("#settings-popup").length && !$(event.target).closest("#settings-toggle").length) {
 				settingsClicked();
